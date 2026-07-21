6522df41f0169f42ef831dfa07e12c08081ec6ad
hndko <hd.doko22@gmail.com>
2026-07-21 23:10:54 +0700

    feat: perbarui logo utama dan favicon aplikasi MariDocs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,7 @@ export function Header() {
   return (
     <header className="header" id="app-header">
       <div className="header__brand">
-        <div className="header__logo" aria-hidden="true">
-          <div className="header__logo-text">
-            <span>Mari</span>
-            <span>Docs</span>
-          </div>
-        </div>
+        <img src="/logo.png" alt="MariDocs Logo" className="header__logo-img" />
         <div>
           <h1 className="header__title">
             Mari<span className="header__title-accent">Docs</span>
